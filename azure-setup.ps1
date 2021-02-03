17b2c3196267fe68d7a2d03d2db6f77a8c697218
Joe Hopper <brkr19@users.noreply.github.com>
2021-02-03 09:02:04 -0600

    Initial script

---
+++ azure-setup.ps1
@@ -0,0 +1,30 @@
+Param (
+    [Parameter(Mandatory=$true,HelpMessage="The tenantId for your Azure subscription")][Alias("tenantId")][string]$tenantIdVal
+)
+
+Write-Output "The tenantId is $tenantIdVal"
+
+# Sign in
+Connect-AzureAD -TenantId $tenantIdVal
+
+# Create the audit app
+$appName = "FractureLabsAuditApp"
+$auditApp = New-AzureADApplication -DisplayName $appName 
+
+# Create the client secret
+$startDate = Get-Date
+$endDate = $startDate.AddMonths(3)
+$auditAppSecret = New-AzureADApplicationPasswordCredential -ObjectId $auditApp.ObjectId -CustomKeyIdentifier "FractureLabsAuditAppSecret" -StartDate $startDate -EndDate $endDate
+Write-Output "The app key is: $($auditAppSecret.Value)"
+
+
+# Create the audit app
+$appName = "FractureLabsAuditApp"
+
+if(!($auditApp = Get-AzADApplication -DisplayName $appName))
+{
+    $auditApp = New-AzADApplication -DisplayName $appName -IdentifierUris "http://$appName"
+}
+
+# Create a Service Principal for the app and provide Read access
+New-AzADServicePrincipal -ApplicationId $auditApp.ApplicationId -Role Reader
